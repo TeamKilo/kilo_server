7bffce78f15ea653a27e47b89aadd71d0511cc56
vincent <vincentyntang@gmail.com>
2022-02-01 14:57:39 +0000

    Begin implementation of game state management for connect 4.

--- src/game/adapter.rs
+++ src/game/adapter.rs
@@ -2,10 +2,35 @@ use serde_json::Value;
 use serde::{Serialize, Deserialize};
 use std::vec::Vec;
 use crate::game::{GameId, SessionId};
-use actix_web::Result;
+use actix_web::{ResponseError, Result};
+use std::fmt;
+use std::fmt::{Display, Formatter};
+use actix_web::http::StatusCode;
 
+#[derive(Debug, Clone)]
+pub enum GameAdapterError {
+    PlayerLimitExceeded(usize),
+    InvalidGameState(State)
+}
 
-#[derive(Serialize)]
+impl fmt::Display for GameAdapterError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            GameAdapterError::PlayerLimitExceeded(size) =>
+                write!(f, "limit of {} players exceeded", size),
+            GameAdapterError::InvalidGameState(state) =>
+                write!(f, "invalid operation for state {}", state)
+        }
+    }
+}
+
+impl ResponseError for GameAdapterError {
+    fn status_code(&self) -> StatusCode {
+        StatusCode::CONFLICT
+    }
+}
+
+#[derive(Serialize, Copy, Clone)]
 #[serde(rename_all = "snake_case")]
 pub enum State {
     Waiting,
@@ -13,12 +38,22 @@ pub enum State {
     Ended,
 }
 
+impl Display for State {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            State::Waiting => write!(f, "waiting"),
+            State::InProgress => write!(f, "in_progress"),
+            State::Ended => write!(f, "ended")
+        }
+    }
+}
+
 #[derive(Serialize)]
 pub struct GenericGameState {
-    players: Vec<String>,
-    state: State,
-    can_move: Vec<String>,
-    payload: Value,
+    pub players: Vec<String>,
+    pub state: State,
+    pub can_move: Vec<String>,
+    pub payload: Value,
 }
 
 #[derive(Deserialize)]
@@ -29,7 +64,7 @@ pub struct GenericGameMove {
 
 pub trait GameAdapter: Send {
     fn new(game_id: GameId) -> Self where Self: Sized;
-    fn add_player(&mut self, session_id: SessionId) -> Result<()>;
-    fn play_move(&mut self, session_id: SessionId, encoded_move: &str) -> Result<()>;
-    fn get_encoded_state(&self) -> String;
+    fn add_player(&mut self, username: String) -> Result<()>;
+    fn play_move(&mut self, game_move: GenericGameMove) -> Result<()>;
+    fn get_encoded_state(&self) -> Result<GenericGameState>;
 }
---
+++ src/game/connect4.rs
@@ -0,0 +1,52 @@
+use crate::game::adapter::{GameAdapter, GenericGameMove, GenericGameState, GameAdapterError, State};
+use crate::game::{GameId, SessionId};
+use std::vec::Vec;
+use std::vec;
+
+const NUM_PLAYERS: usize = 2;
+
+struct Connect4Adapter<'a> {
+    game_id: GameId,
+    players: Vec<String>,
+    state: State,
+    next_move: &'a String,
+    board: Vec<Vec<&'a String>> // vector of columns, each variable length.
+}
+
+impl GameAdapter for Connect4Adapter {
+    fn new(game_id: GameId) -> Self where Self: Sized {
+        Connect4Adapter {
+            game_id,
+            players: vec![],
+            state: State::Waiting,
+            next_move: "".parse().unwrap(),
+            board: vec![]
+        }
+    }
+
+    fn add_player(&mut self, username: String) -> actix_web::Result<()> {
+        if self.players.len() < NUM_PLAYERS {
+            self.players.push(username);
+            if self.players.len() == NUM_PLAYERS { self.state = State::InProgress }
+            Ok(())
+        } else {
+            Err(actix_web::Error::from(GameAdapterError::PlayerLimitExceeded(NUM_PLAYERS)))
+        }
+    }
+
+    fn play_move(&mut self, game_move: GenericGameMove) -> actix_web::Result<()> {
+        if self.state != State::InProgress {
+            Err(actix_web::Error::from(GameAdapterError::InvalidGameState(self.state)))
+        }
+        todo!()
+    }
+
+    fn get_encoded_state(&self) -> actix_web::Result<GenericGameState> {
+        Ok(GenericGameState {
+            players: self.players.clone(),
+            state: self.state,
+            can_move: vec![self.next_move.clone()],
+            payload: serde_json::to_value(&self.board)?
+        })
+    }
+}
