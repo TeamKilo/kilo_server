0c8c30112668922fe163d0e6fc307c7e73d1252b
vincent <vincentyntang@gmail.com>
2022-02-01 16:00:50 +0000

    Fix compile errors and add host/port config with environment variables.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ mod game;
 
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use std::sync::RwLock;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let host = env::var("HOST").unwrap_or("127.0.0.1".to_string());
+    let port = env::var("PORT").unwrap_or("8080".to_string());
+
     let game_manager = web::Data::new(
         RwLock::new(game::GameManager::new())
     );
@@ -18,5 +22,5 @@ async fn main() -> std::io::Result<()> {
             .service(api::get_state)
             .service(api::submit_move)
             .service(api::wait_for_move)
-    }).bind(("127.0.0.1", 8080))?.run().await
+    }).bind(format!("{}:{}", host, port))?.run().await
 }
