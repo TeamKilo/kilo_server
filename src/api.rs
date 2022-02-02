0aec7b84ed2d29bb3304359c995f73ff5cca5da1
Theo Wang <31027215+MilkFansHelloWorld@users.noreply.github.com>
2022-02-02 22:29:06 +0000

    API Entries: implemented all but list and wait (#3)

--- src/api.rs
+++ src/api.rs
@@ -1,33 +1,135 @@
 // API endpoints
 
-use actix_web::{post, get, Responder, web::Payload};
+use crate::game::adapter::GameAdapter;
+use crate::game::ValidationError::NoSuchGameError;
+use crate::game::{adapter, connect4, GameId, GameManager};
+use actix_web::web::Json;
+use actix_web::{get, post, web, Error, Result};
+use serde::{Deserialize, Serialize};
+use std::sync::RwLock;
+
+/* Helpers */
+
+/* Structs for the different endpoints*/
+
+/// Struct for req to `/api/create-game`
+#[derive(Deserialize)]
+pub struct CreateGameReq {
+    name: String,
+}
+
+/// Struct for res from `/api/create-game`
+#[derive(Serialize)]
+pub struct CreateGameRes {
+    game_id: String,
+}
+
+/// Struct for res from `/api/list-games`
+#[derive(serde::Serialize)]
+pub struct ListGamesRes {
+    // TODO
+}
+
+/// Struct for req to `/api/join-game`
+#[derive(Deserialize)]
+pub struct JoinGameReq {
+    game_id: String,
+    username: String,
+}
+
+/// Struct for res from `/api/join-game`
+#[derive(serde::Serialize)]
+pub struct JoinGameRes {
+    session_id: String,
+}
+
+// Struct for res from `/api/{game_id}/get-state`
+// <br> It's basically the game::adaptor::GenericGameState Object
+
+// Struct for req to `/api/{game_id}/submit-move`
+// This is basically the game::adaptor::GenericGameMove struct
+
+/// Struct for res from `/api/{game_id}/submit-move`
+#[derive(Serialize)]
+pub struct SubmitMoveRes {
+    success: bool,
+}
+
+/// Struct for res from `/api/{game_id}/submit-move`
+#[derive(Serialize)]
+pub struct WaitForUpdateRes {
+    updated: bool,
+}
 
 #[post("/api/create-game")]
-pub(crate) async fn create_game(mut payload: Payload) -> impl Responder {
-    todo!(); "not yet implemented"
+pub(crate) async fn create_game(
+    mut payload: web::Json<CreateGameReq>,
+    gm_wrapped: web::Data<RwLock<GameManager>>,
+) -> Result<Json<CreateGameRes>> {
+    // Validate & Pass it on
+    let mut gm = gm_wrapped.write().unwrap();
+    let game_id = match payload.name.as_str() {
+        "connect_4" => gm.create_game(|id| Box::new(connect4::Connect4Adapter::new(id))),
+        _ => return Err(Error::from(NoSuchGameError(payload.name.clone()))),
+    };
+    game_id.and_then(|id| {
+        Ok(Json(CreateGameRes {
+            game_id: id.to_string(),
+        }))
+    })
 }
 
 #[get("/api/list-games")]
-pub(crate) async fn list_games(mut payload: Payload) -> impl Responder {
-    todo!(); "not yet implemented"
+pub(crate) async fn list_games() -> Result<Json<ListGamesRes>> {
+    todo!()
 }
 
 #[post("/api/join-game")]
-pub(crate) async fn join_game(mut payload: Payload) -> impl Responder {
-    todo!(); "not yet implemented"
+pub(crate) async fn join_game(
+    mut payload: web::Json<JoinGameReq>,
+    gm_wrapped: web::Data<RwLock<GameManager>>,
+) -> Result<Json<JoinGameRes>> {
+    // Validate input
+    let game_id = GameId::from(&payload.game_id)?;
+    // Join a game
+    let mut gm = gm_wrapped.write().unwrap();
+    gm.receive_join(game_id, payload.username.clone())
+        .and_then(|session_id| {
+            Ok(Json(JoinGameRes {
+                session_id: session_id.to_string(),
+            }))
+        })
 }
 
 #[get("/api/{game_id}/get-state")]
-pub(crate) async fn get_state(mut payload: Payload) -> impl Responder {
-    todo!(); "not yet implemented"
+pub(crate) async fn get_state(
+    web::Path((game_id)): web::Path<String>,
+    gm_wrapped: web::Data<RwLock<GameManager>>,
+) -> Result<Json<adapter::GenericGameState>> {
+    // Validation & get_state
+    let game_id = GameId::from(&game_id)?;
+    let gm = gm_wrapped.read().unwrap();
+    // Get state and return
+    Ok(Json(gm.get_state(game_id)?))
 }
 
 #[post("/api/{game_id}/submit-move")]
-pub(crate) async fn submit_move(mut payload: Payload) -> impl Responder {
-    todo!(); "not yet implemented"
+pub(crate) async fn submit_move(
+    web::Path((game_id)): web::Path<String>,
+    mut payload: web::Json<adapter::GenericGameMove>,
+    gm_wrapped: web::Data<RwLock<GameManager>>,
+) -> Result<Json<SubmitMoveRes>> {
+    let game_id = GameId::from(&game_id)?;
+    // Submit that to game manager
+    let mut gm = gm_wrapped.read().unwrap();
+    // Return success or failure
+    gm.receive_move(payload.into_inner())
+        .and_then(|()| Ok(Json(SubmitMoveRes { success: true })))
 }
 
 #[get("/api/{game_id}/wait-for-update")]
-pub(crate) async fn wait_for_move(mut payload: Payload) -> impl Responder {
-    todo!(); "not yet implemented"
+pub(crate) async fn wait_for_move(
+    web::Path((game_id)): web::Path<String>,
+) -> Result<Json<WaitForUpdateRes>> {
+    todo!()
 }
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,16 +1,40 @@
-mod adapter;
-mod connect4;
+pub mod adapter;
+pub mod connect4;
 
+use crate::game::adapter::{GenericGameMove, GenericGameState};
+use crate::game::ValidationError::ParseIdError;
+use actix_web::http::StatusCode;
+use actix_web::{Error, ResponseError, Result};
 use adapter::GameAdapter;
+use rand::Rng;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fmt;
-use std::fmt::Formatter;
+use std::fmt::{Display, Formatter};
 use std::ops::DerefMut;
 use std::sync::Mutex;
-use serde::{Serialize, Deserialize};
-use actix_web::{ResponseError, Result};
-use actix_web::http::StatusCode;
-use rand::Rng;
+
+/// ValidationError
+#[derive(Debug, Clone)]
+pub enum ValidationError {
+    ParseIdError(String),
+    NoSuchGameError(String),
+}
+
+impl Display for ValidationError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ValidationError::ParseIdError(id) => write!(f, "ID {} is Invalid", id),
+            ValidationError::NoSuchGameError(game) => write!(f, "Game {} not found", game),
+        }
+    }
+}
+
+impl ResponseError for ValidationError {
+    fn status_code(&self) -> StatusCode {
+        StatusCode::BAD_REQUEST
+    }
+}
 
 #[derive(Copy, Clone, Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
 pub struct GameId(u128);
@@ -24,11 +48,31 @@ impl GameId {
         let id: u128 = rng.gen();
         GameId(id)
     }
+    // Added for API to create a GameId object to input to the GameManager
+    pub fn from(id: &String) -> Result<Self> {
+        GameId::validate_id(id).and_then(|id| Ok(GameId(id)))
+    }
+
+    /// validate_id
+    fn validate_id(game_id: &String) -> Result<u128, Error> {
+        if !game_id.starts_with("game_") {
+            return Err(actix_web::Error::from(ParseIdError(game_id.clone())));
+        }
+        let game_id_int = u128::from_str_radix(&game_id[5..], 10);
+        match game_id_int {
+            Ok(value) => Ok(value),
+            Err(_) => {
+                return Err(actix_web::Error::from(ValidationError::ParseIdError(
+                    game_id.clone(),
+                )))
+            } // TODO: need custom error
+        }
+    }
 }
 
 impl fmt::Display for GameId {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "Game{}", self.0)
+        write!(f, "game_{}", self.0)
     }
 }
 
@@ -38,11 +82,31 @@ impl SessionId {
         let id: u128 = rng.gen();
         SessionId(id)
     }
+    // Added for API to create a SessionId object
+    pub fn from(id: u128) -> Self {
+        SessionId(id)
+    }
+
+    /// validate_id
+    fn validate_id(session_id: &String) -> Result<u128, Error> {
+        if !session_id.starts_with("session_") {
+            return Err(actix_web::Error::from(ParseIdError(session_id.clone())));
+        }
+        let session_id_int = u128::from_str_radix(&session_id[8..], 10);
+        match session_id_int {
+            Ok(value) => Ok(value),
+            Err(_) => {
+                return Err(actix_web::Error::from(ValidationError::ParseIdError(
+                    session_id.clone(),
+                )))
+            } // TODO: need custom error
+        }
+    }
 }
 
 impl fmt::Display for SessionId {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "Session{}", self.0)
+        write!(f, "session_{}", self.0)
     }
 }
 
@@ -55,10 +119,12 @@ pub enum GameManagerError {
 impl fmt::Display for GameManagerError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            GameManagerError::GameIdDoesNotExist(game_id) =>
-                write!(f, "game corresponding to {} does not exist", game_id),
-            GameManagerError::SessionIdDoesNotExist(session_id) =>
+            GameManagerError::GameIdDoesNotExist(game_id) => {
+                write!(f, "game corresponding to {} does not exist", game_id)
+            }
+            GameManagerError::SessionIdDoesNotExist(session_id) => {
                 write!(f, "session corresponding to {} does not exist", session_id)
+            }
         }
     }
 }
@@ -67,7 +133,7 @@ impl ResponseError for GameManagerError {
     fn status_code(&self) -> StatusCode {
         match self {
             GameManagerError::GameIdDoesNotExist(_) => StatusCode::NOT_FOUND,
-            GameManagerError::SessionIdDoesNotExist(_) => StatusCode::NOT_FOUND
+            GameManagerError::SessionIdDoesNotExist(_) => StatusCode::NOT_FOUND,
         }
     }
 }
@@ -79,10 +145,7 @@ pub struct Session {
 
 impl Session {
     pub fn new(username: String, game_id: GameId) -> Self {
-        Session {
-            username,
-            game_id,
-        }
+        Session { username, game_id }
     }
 }
 
@@ -93,10 +156,16 @@ pub struct GameManager {
 
 impl GameManager {
     pub fn new() -> Self {
-        GameManager { games: HashMap::new(), sessions: HashMap::new() }
+        GameManager {
+            games: HashMap::new(),
+            sessions: HashMap::new(),
+        }
     }
 
-    pub fn create_game(&mut self, game: impl FnOnce(GameId) -> Box<dyn GameAdapter>) -> Result<GameId> {
+    pub fn create_game(
+        &mut self,
+        game: impl FnOnce(GameId) -> Box<dyn GameAdapter>,
+    ) -> Result<GameId> {
         let mut game_id;
         loop {
             game_id = GameId::new();
@@ -112,7 +181,9 @@ impl GameManager {
 
     pub fn receive_join(&mut self, game_id: GameId, username: String) -> Result<SessionId> {
         if !self.games.contains_key(&game_id) {
-            return Err(actix_web::Error::from(GameManagerError::GameIdDoesNotExist(game_id)))
+            return Err(actix_web::Error::from(
+                GameManagerError::GameIdDoesNotExist(game_id),
+            ));
         }
 
         let mut session_id;
@@ -134,25 +205,30 @@ impl GameManager {
         Ok(session_id)
     }
 
-    // pub fn receive_move(&self, session_id: SessionId, encoded_move: ?) -> Result<()> {
-    //     todo!()
-    // }
-    //
-    // pub fn get_state(&self, game_id: GameId) -> Result<?> {
-    //     todo!()
-    // }
+    pub fn receive_move(&self, encoded_move: GenericGameMove) -> Result<()> {
+        // Deleted sessionId because it corresponds to the "player" field in GenericGameMove
+        todo!()
+    }
+
+    pub fn get_state(&self, game_id: GameId) -> Result<GenericGameState> {
+        todo!()
+    }
 
     fn get_game_adapter(&self, game_id: GameId) -> Result<&Mutex<Box<dyn GameAdapter>>> {
         match self.games.get(&game_id) {
             Some(x) => Ok(x),
-            None => Err(actix_web::Error::from(GameManagerError::GameIdDoesNotExist(game_id)))
+            None => Err(actix_web::Error::from(
+                GameManagerError::GameIdDoesNotExist(game_id),
+            )),
         }
     }
 
     fn get_session(&self, session_id: SessionId) -> Result<&Session> {
         match self.sessions.get(&session_id) {
             Some(x) => Ok(x),
-            None => Err(actix_web::Error::from(GameManagerError::SessionIdDoesNotExist(session_id)))
+            None => Err(actix_web::Error::from(
+                GameManagerError::SessionIdDoesNotExist(session_id),
+            )),
         }
     }
 }
